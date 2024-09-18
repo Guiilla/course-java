a02e4d0de27d6971eb57533c18f32c6d9e5b665a
Guiilla <ingaamable@gmail.com>
2024-09-17 23:29:21 -0500

    segunda clase de java, datos primitivos

---
+++ src/main/java/com/mycompany/course/java/TiposDeDatos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.course.java;
+
+/**
+ *
+ * @author ingaa
+ */
+public class TiposDeDatos {
+    public static void main(String[] args) {
+        
+        String nombre = "Sebastian";
+        int edad = 24;
+        double altura = 1.61;
+        boolean hoyTrabajo = false;
+        
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(hoyTrabajo);
+    }
+}
